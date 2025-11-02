260c2f2dc54e02aa0dcc4acb6d672e0ec3896fe4
AshrafHosam <ashraf.hosam.ibrahim@gmail.com>
2025-11-02 20:34:58 +0200

    Add project files.

---
+++ BoscoStay.ApartmentsManagement.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace BoscoStay.ApartmentsManagement.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
